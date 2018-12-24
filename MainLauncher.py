8c1cf8716fdf5405995685799aaa704a7a5bbeca
jforjohn <giannisotiro@gmail.com>
2018-12-24 18:33:47 +0200

    LAUNCHER: add gridsearch tuning option

--- MainLauncher.py
+++ MainLauncher.py
@@ -8,8 +8,12 @@ import sys
 from os import walk
 from scipy.stats import friedmanchisquare
 import seaborn as sns
-from exercise1_svm import mainExercise1, plot_confusion_matrix
+from exercise1_svm import mainExercise1
 from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from MidpointNormalize import MidpointNormalize
+
+
 
 import numpy as np
 import pandas as pd
@@ -37,6 +41,19 @@ def labelFactorization(ytrain_lbl, ytest_lbl):
     labels_fac = pd.factorize(labels)[0]
     return labels_fac[:ytrain_lbl.size], labels_fac[ytrain_lbl.size:]
 
+
+def plotHeatmap(scores, gamma_range, C_range, dataset, fig_num):
+    plt.figure(10+fig_num, figsize=(8, 6))
+    plt.subplots_adjust(left=.2, right=0.95, bottom=0.15, top=0.95)
+    plt.imshow(scores, interpolation='nearest', cmap=plt.cm.hot,
+               norm=MidpointNormalize(vmin=0.2, midpoint=0.9))
+    plt.xlabel('gamma')
+    plt.ylabel('C')
+    plt.colorbar()
+    plt.xticks(np.arange(len(gamma_range)), gamma_range, rotation=45)
+    plt.yticks(np.arange(len(C_range)), C_range)
+    plt.title(dataset + ', ' + kernel + ':' + ' Validation accuracy')
+
 ##
 if __name__ == '__main__':
     ##
@@ -54,6 +71,7 @@ if __name__ == '__main__':
     dataset = config.get('svm', 'dataset')
     svm_exercise = config.get('svm', 'svm_exercise')
     kernels = config.get('svm', 'kernels').split('-')
+    tuning = config.get('svm', 'tuning')
 
     if svm_exercise == '1':
         datasets = dataset.split('-')
@@ -73,10 +91,55 @@ if __name__ == '__main__':
         accum_acc_lst = []
         accum_time_lst = []
         row_names = []
+        C = 0
+        gamma = 0
         for kernel in kernels:
+            print()
+            print('======================================')
+            print(kernel)
+            print()
+            best_params = {}
             acc_lst = []
             time_lst = []
             start = time()
+            if tuning == 'gridsearch':
+                df_train, ytrain_lbl = getProcessedData(path, dataset, filenames[1])
+                df_test, ytest_lbl = getProcessedData(path, dataset, filenames[0])
+                ytrain, ytest = labelFactorization(ytrain_lbl, ytest_lbl)
+                X = pd.concat([df_train, df_test],
+                               axis=0).reset_index(drop=True)
+                y= np.hstack((ytrain, ytest))
+
+                n_features = X.shape[1]
+                gamma_min = 1.0 / n_features
+                gamma_max = 1.0 / (n_features * X.values.std())
+
+                '''
+                gamma_range = [gamma_min - gamma_mean, gamma_min,
+                               gamma_min + gamma_mean, gamma_max,
+                               gamma_max + gamma_mean]
+                '''
+                gamma_range = np.logspace(-5, 1.2, 10)
+                C_range = np.logspace(-1, 5, 10)
+                param_grid = dict(C=C_range, gamma=gamma_range)
+                clf = GridSearchCV(SVC(kernel=kernel),
+                                   param_grid=param_grid,
+                                   cv=5,
+                                   scoring='accuracy',
+                                   n_jobs=-1,
+                                   error_score='raise',
+                                   iid=False)
+                clf.fit(X, y)
+
+                mean_test_score = clf.cv_results_['mean_test_score']
+                scores = mean_test_score.reshape(len(C_range),
+                                                 len(gamma_range))
+                best_params = clf.best_params_
+                ind = clf.cv_results_['rank_test_score'][0]
+                print('Best params:', best_params, mean_test_score[ind])
+                print()
+                plotHeatmap(scores, gamma_range, C_range, dataset, len(row_names))
+
             for f_ind in range(0,len(filenames), 2):
                 test_file = filenames[f_ind]
                 train_file = filenames[f_ind+1]
@@ -85,6 +148,7 @@ if __name__ == '__main__':
                 df_test, ytest_lbl = getProcessedData(path, dataset, test_file)
                 ytrain, ytest = labelFactorization(ytrain_lbl, ytest_lbl)
 
+                # fix missing columns because of NaNs and one hot encoding without dummy_na
                 if df_train.shape[1] != df_test.shape[1]:
                     missing_cols = set(df_test.columns) - set(df_train.columns)
                     for col in missing_cols:
@@ -95,10 +159,14 @@ if __name__ == '__main__':
                         print(df_train.shape, df_test.shape)
                         df_test[col] = np.zeros([df_test.shape[0],1])
 
+                C = best_params.get('C', 42)
+                # gamma from best params or 1/n_features: a
+                gamma = best_params.get('gamma', 1.0/df_train.shape[1])
+
                 clf = SVC(kernel=kernel,
                           random_state=42,
-                          gamma='auto',
-                          C=42)
+                          gamma=gamma,
+                          C=C)
                 clf.fit(df_train, ytrain)
 
                 acc = clf.score(df_test, ytest)
@@ -106,7 +174,9 @@ if __name__ == '__main__':
                 duration = time() - start
                 time_lst.append(duration)
 
-            row_name = 'kernel:%s/C:%.2f/gamma:%.2f' %(kernel, clf.C, clf._gamma)
+            row_name = 'kernel:%s/C:%.2f/gamma:%.5f' % (kernel,
+                                                        C,
+                                                        gamma)
             row_names.append(row_name)
             accum_acc_lst.append(acc_lst)
             accum_time_lst.append(time_lst)
